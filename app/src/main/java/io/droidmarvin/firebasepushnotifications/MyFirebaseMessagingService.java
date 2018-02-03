1b8d57403fe4fc33ffbd1c41d7b0bc8fffe6be03
ngesa marvin <marvinngesa@gmail.com>
2018-02-03 21:58:12 +0300

    update MyFirebaseMessagingService class

--- app/src/main/java/io/droidmarvin/firebasepushnotifications/MyFirebaseMessagingService.java
+++ app/src/main/java/io/droidmarvin/firebasepushnotifications/MyFirebaseMessagingService.java
@@ -1,8 +1,14 @@
 package io.droidmarvin.firebasepushnotifications;
 
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
 /**
  * Created by User on 2/3/2018.
  */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-public class MyFirebaseMessagingService {
+    @Override public void onMessageReceived(RemoteMessage remoteMessage) {
+
+    }
 }
